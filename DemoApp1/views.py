260e285b8e1d3abf73bb94f3373b38b514059c6c
pavan24032000 <kumar24032000@gmail.com>
2023-01-03 21:22:39 +0530

    second commit

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -7,3 +7,5 @@ def f1(request):
 def f2(request):
 	return HttpResponse("<h1>Hello from DemoApp1 f2()</h1><hr />");
 
+def f11(request):
+	return HttpResponse("<h1>Hello from DemoApp1 f11()</h1><hr />");
